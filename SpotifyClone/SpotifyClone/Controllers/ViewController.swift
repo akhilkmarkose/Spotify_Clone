ad6781e05b0a0dcc28ac40921f09c823574927ac
akhilkmarkose <akhilkmarkose@gmail.com>
2024-11-23 18:28:03 +0530

    Initial Commit - Project Initialization

---
+++ SpotifyClone/SpotifyClone/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by AKHIL KUNJUMON MARKOSE on 23/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ SpotifyClone/SpotifyCloneTests/SpotifyCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneTests.swift
+//  SpotifyCloneTests
+//
+//  Created by AKHIL KUNJUMON MARKOSE on 23/11/24.
+//
+
+import Testing
+@testable import SpotifyClone
+
+struct SpotifyCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
